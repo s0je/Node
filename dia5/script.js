8ff1d3eb881812d6a91405e9204eb82d78d47365
s0je <cuentasegurajss@gmail.com>
2022-02-14 12:44:45 +0100

    Imdb Server Update

--- dia5/script.js
+++ dia5/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 });
 class Professional{
 
-  constructor(id, image, name, age, genre, weight, height, hairColor, race, isRetired, nationality, oscarsNumber, profesion)
+  constructor( image, name, age, genre, weight, height, hairColor, race, isRetired, nationality, oscarsNumber, profesion)
   {   
       this.image = image;
       this.name = name;
@@ -29,8 +29,12 @@ class Professional{
 
 async function mostrar()
 {
+  let mostrar = document.getElementById("buscar");
+  mostrar.style.cssText = 'visibility: visible;	display: block;';
   let borrador = document.getElementById("borrador");
   borrador.style.cssText = 'visibility: hidden;	display: none;';
+  let img = document.getElementById("img");
+  img.style.cssText = 'visibility: hidden;	display: none;';
     let form = document.getElementById("formulario");
     form.className = 'formulario_oculto';
     form.style.cssText = 'width: 100%; display: flex;  justify-content: center;visibility: hidden;	display: none;';
@@ -46,43 +50,85 @@ async function mostrar()
         let resul = await data.json()
         let textTemp='';
         console.log(resul);
-        if(resul.mensaje === 'No hay profesionales')
-        {
-          
-        }
-        else {
-          for(let prof of resul)
-            {
-              textTemp +=`<div class="col">
-                            <div class="card" style="width: 18rem;">
-                                <img src="`+prof.image+`" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <div class="card-header">
-                                    `+prof.name+`
-                                    </div>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Genre: `+prof.genre+`</li>
-                                        <li class="list-group-item">Age: `+prof.age+`</li>
-                                        <li class="list-group-item">Weigth: `+prof.weight+`</li>
-                                        <li class="list-group-item">Height: `+prof.height+`</li>
-                                        <li class="list-group-item">Hair color: `+prof.hairColor+`</li>
-                                        <li class="list-group-item">Race: `+prof.race+`</li>
-                                        <li class="list-group-item">Is retides?: `+prof.isRetired+`</li>
-                                        <li class="list-group-item">Nationality: `+prof.nationality+`</li>
-                                        <li class="list-group-item">Oscar's: `+prof.oscarsNumber+`</li>
-                                        <li class="list-group-item">Profesion: `+prof.profesion+`</li>
-                                    </ul>
-                                </div>
-                            </div>
-                          </div>`;
-            }
+        for(let prof of resul)
+          {
+            textTemp +=`<div class="col">
+                          <div class="card" style="width: 18rem;">
+                              <img src="`+prof.image+`" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                  <div class="card-header">
+                                  `+prof.name+`
+                                  </div>
+                                  <ul class="list-group list-group-flush">
+                                      <li class="list-group-item">Genre: `+prof.genre+`</li>
+                                      <li class="list-group-item">Age: `+prof.age+`</li>
+                                      <li class="list-group-item">Weigth: `+prof.weight+`</li>
+                                      <li class="list-group-item">Height: `+prof.height+`</li>
+                                      <li class="list-group-item">Hair color: `+prof.hairColor+`</li>
+                                      <li class="list-group-item">Race: `+prof.race+`</li>
+                                      <li class="list-group-item">Is retides?: `+prof.isRetired+`</li>
+                                      <li class="list-group-item">Nationality: `+prof.nationality+`</li>
+                                      <li class="list-group-item">Oscar's: `+prof.oscarsNumber+`</li>
+                                      <li class="list-group-item">Profesion: `+prof.profesion+`</li>
+                                  </ul>
+                              </div>
+                          </div>
+                        </div>`;
+          }
+          document.getElementById("profesional").innerHTML = textTemp;
         }
         
-        document.getElementById("profesional").innerHTML = textTemp;
-    }catch(err) {
-      console.log(err);
-  }
+    catch(err) 
+        {
+          console.log(err);
+        }
 }
+
+async function mostrarId()
+{
+  document.getElementById("profesional").innerHTML = '';
+  let buscar = document.getElementById("buscarId").value;
+  let url = 'http://localhost:5500/profesionales'
+    let param = {
+        headers:{"content-type": "application/json; charset=UTF-8"},
+        method:"GET"
+    }
+    try {
+        let data = await fetch(url,param)
+        let resul = await data.json()
+        let textTemp='';
+        console.log(resul);
+            textTemp +=`<div class="col">
+                          <div class="card" style="width: 18rem;">
+                              <img src="`+resul[buscar].image+`" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                  <div class="card-header">
+                                  `+resul[buscar].name+`
+                                  </div>
+                                  <ul class="list-group list-group-flush">
+                                      <li class="list-group-item">Genre: `+resul[buscar].genre+`</li>
+                                      <li class="list-group-item">Age: `+resul[buscar].age+`</li>
+                                      <li class="list-group-item">Weigth: `+resul[buscar].weight+`</li>
+                                      <li class="list-group-item">Height: `+resul[buscar].height+`</li>
+                                      <li class="list-group-item">Hair color: `+resul[buscar].hairColor+`</li>
+                                      <li class="list-group-item">Race: `+resul[buscar].race+`</li>
+                                      <li class="list-group-item">Is retides?: `+resul[buscar].isRetired+`</li>
+                                      <li class="list-group-item">Nationality: `+resul[buscar].nationality+`</li>
+                                      <li class="list-group-item">Oscar's: `+resul[buscar].oscarsNumber+`</li>
+                                      <li class="list-group-item">Profesion: `+resul[buscar].profesion+`</li>
+                                  </ul>
+                              </div>
+                          </div>
+                        </div>`;
+          document.getElementById("profesional").innerHTML = textTemp;
+        }
+        
+    catch(err) 
+        {
+          console.log(err);
+        }
+}
+
 async function postProfesional()
 {
   try
@@ -186,6 +232,8 @@ async function delProfesional()
 }
 function crearProfesional()
 {
+  let mostrar = document.getElementById("buscar");
+  mostrar.style.cssText = 'visibility: hidden;	display: none;'
   document.getElementById("profesional").innerHTML = '';
   let borrador = document.getElementById("borrador");
   borrador.style.cssText = 'visibility: hidden;	display: none;'  
@@ -202,6 +250,8 @@ function crearProfesional()
 }
 function update()
 {
+  let mostrar = document.getElementById("buscar");
+  mostrar.style.cssText = 'visibility: hidden;	display: none;'
   document.getElementById("profesional").innerHTML = '';
   let borrador = document.getElementById("borrador");
   borrador.style.cssText = 'visibility: hidden;	display: none;';
@@ -214,14 +264,13 @@ function update()
   let add = document.getElementById("add");
   add.style.cssText= "margin-top: 15px; margin-bottom: 15px; visibility: hidden; display: none";
   let update = document.getElementById("up");
-  update.style.cssText = "margin-top: 15px; margin-bottom: 15px; visibility: visible; display: block";
-  
-
-  
+  update.style.cssText = "margin-top: 15px; margin-bottom: 15px; visibility: visible; display: block"; 
 }
 
 function borrar()
 {
+  let mostrar = document.getElementById("buscar");
+  mostrar.style.cssText = 'visibility: hidden;	display: none;'
   document.getElementById("profesional").innerHTML = '';
   let form = document.getElementById("formulario");
   form.style.cssText = 'width: 100%; display: flex;  justify-content: center;visibility: hidden;	display: none;';
