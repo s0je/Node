a986c6a6824246a1dbbb7b28295d48acdf2d08dd
s0je <cuentasegurajss@gmail.com>
2022-02-14 15:54:54 +0100

    Api Pokemon

--- ApiPokemon/script.js
+++ ApiPokemon/script.js
@@ -21,83 +21,87 @@ const colors = {
 
 const color_types = Object.keys(colors)
 
-async function getPokemon(id)
+
+async function crearCard(pokemon)
 {
     try
     {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        let id = document.getElementById("id").value;
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-    let response = await fetch(url);
-    let data = await response.json();
-    crearCard(data);
+        let response = await fetch(url);
+        let data = await response.json();
+
+        const pokeCard = document.createElement('div')
+        pokeCard.classList.add('pokemon')
+        const name = data.name[0].toUpperCase() + data.name.slice(1)
+        const e = data.id.toString().padStart(3, '0')
+        const poke_types = data.types.map(type => type.type.name)
+        const type = color_types.find(type => poke_types.indexOf(type) > -1)    
+        const color = colors[type]
+
+        pokeCard.style.backgroundColor = color
+
+        const pokeTemp = `
+        <div class="img-container">
+            <img src="${data.sprites.front_default}" alt="">
+        </div>
+        <div class="info">
+            <span class="number">#${e}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type: <span>${type}</span> </small>
+        </div>
+        `
+        let temp = '';
+        let j = 1;
+        let i = 0;
+        for(let abi of data.abilities)
+        {
+            
+            temp += 
+                    `<th scope="row">${j}</th>
+                    <td>${abi.ability.name}</td> `
+            j++;
+        }
+        console.log(temp);
+        let table = `<table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Habilidad</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr id="habilidad">
+                         ${temp}                         
+                        </tr>
+                        </tbody>
+                    </table>`
+        pokeCard.innerHTML = pokeTemp + table
+        poke_container.appendChild(pokeCard)
     }
     catch(err)
     {
         console.log(err);
     }
+    
+   
 }
 
-function init()
+async function abilities(name)
 {
-
-    let id = document.getElementById("id").value;
-    const pokemon = getPokemon(id);
-    console.log(pokemon);
-
+    try
+    {
+        let url = `https://pokeapi.co/api/v2/ability/${name}/`
+        let response = await fetch(url);
+        let data = await response.json();
+        
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
 }
-function crearCard(pokemon)
-{
-    const pokeCard = document.createElement('div')
-    pokeCard.classList.add('pokemon')
-    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
-    const ability1 = pokemon.abilities[0].ability.name;
-    const id = pokemon.id.toString().padStart(3, '0')
-    const poke_types = pokemon.types.map(type => type.type.name)
-    const type = color_types.find(type => poke_types.indexOf(type) > -1)    
-    const color = colors[type]
-
-    pokeCard.style.backgroundColor = color
-
-    const pokeTemp = `
-    <div class="img-container">
-        <img src="${pokemon.sprites.front_default}" alt="">
-    </div>
-    <div class="info">
-        <span class="number">#${id}</span>
-        <h3 class="name">${name}</h3>
-        <small class="type">Type: <span>${type}</span> </small>
-    </div>
-    <div>
-    <table class="table">
-    <thead>
-        <tr>
-        <th scope="col">#</th>
-        <th scope="col">Habilidad 1</th>
-        </tr>
-    </thead>
-    <tbody id="tBody">
-        <tr>
-        <th scope="row">Habilidad</th>
-        <td>${ability1}</td>
-        </tr>
-    </tbody>
-    </table>
-    </div>
-    `
-    pokeCard.innerHTML = pokeTemp
-    poke_container.appendChild(pokeCard)
-}
-
-function addInfo(valor)
-{
-    let tbody = document.getElementById("tBody")
-    let temp = `<tr>
-    <th scope="row">Descripcion</th>
-    <td scope="col">${valor}</td>
-    </tr>`
-    tbody.innerHTML = temp;
-}
-
 
 function vaciar()
 {
